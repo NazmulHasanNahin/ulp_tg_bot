21c317ce68bbc26530786d2fcf671370e07e2ea9
NazmulHasanNahin <nahinn75@gmail.com>
2025-05-28 22:14:44 +0600

    env

--- bot.py
+++ bot.py
@@ -168,14 +168,16 @@ async def main():
     
     
 import asyncio
+import nest_asyncio
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except RuntimeError as e:
-        if "event loop is already running" in str(e):
-            loop = asyncio.get_event_loop()
-            loop.create_task(main())
-            loop.run_forever()
+        if "This event loop is already running" in str(e):
+            
+            nest_asyncio.apply()
+            asyncio.get_event_loop().run_until_complete(main())
         else:
             raise
+
